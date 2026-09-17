0a0dd7fca0030a81bc25bd3ce83149d8581aacf4
reymono <adwind007@gmail.com>
2026-09-17 10:35:16 -0600

    Customize portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
+	site: "https://codigoreymono.github.io",
+
 	vite: {
 		plugins: [tailwindcss()],
 		resolve: {
